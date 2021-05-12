2c31752d41a21e168200db33bae65b558c59f543
Francisco J. Peña <frape@kth.se>
2021-05-12 13:58:26 +0200

    Updated the unif_main.py module to include more versions of UNIF

--- unif/unif_main.py
+++ unif/unif_main.py
@@ -4,7 +4,8 @@ import utils
 from unif.unif_evaluate import evaluate_top_n
 from unif.unif_plots import plot
 from unif.unif_random_model import RandomModel
-from unif.unif_train import train_cycle
+from unif import unif_train_triplet, unif_train_cosine_neg, unif_train_cosine_pos_neg
+from unif import unif_train_cosine_pos
 
 
 def test_random_model():
@@ -13,14 +14,48 @@ def test_random_model():
 
 
 def test_unif_model():
-    unif_model, current_loss, all_losses = train_cycle()
+    unif_model, current_loss, all_losses = unif_train_triplet.train_cycle()
     plot(all_losses)
     evaluate_top_n(unif_model)
 
 
+def test_unif_cosine_pos_model():
+    unif_model, current_loss, all_losses = unif_train_cosine_pos.train_cycle(True)
+    plot(all_losses)
+    evaluate_top_n(unif_model)
+
+
+def test_unif_cosine_neg_model():
+    unif_model, current_loss, all_losses = unif_train_cosine_neg.train_cycle(True)
+    # plot(all_losses)
+    # evaluate_top_n(unif_model)
+
+
+def test_unif_cosine_pos_neg_model():
+    unif_model, current_loss, all_losses = unif_train_cosine_pos_neg.train_cycle(True)
+    # plot(all_losses)
+    # evaluate_top_n(unif_model)
+
+
+def test_unif_cosine_pos_neg_random_model():
+    unif_model, current_loss, all_losses = unif_train_cosine_pos_neg.train_cycle(True)
+    # plot(all_losses)
+    # evaluate_top_n(unif_model)
+
+
+def test_unif_triplet_model():
+    unif_model, current_loss, all_losses = unif_train_triplet.train_cycle(True)
+    # plot(all_losses)
+    # evaluate_top_n(unif_model)
+
+
 def main():
     utils.plant_seeds()
-    test_unif_model()
+    # test_unif_model()
+    # test_unif_cosine_pos_model()
+    # test_unif_cosine_neg_model()
+    # test_unif_cosine_pos_neg_model()
+    test_unif_triplet_model()
     # test_random_model()
 
 
